4b5f884b2df88f391eb3ef965aae37761c002586
PepeTonin <phavilat@hotmail.com>
2023-08-19 09:44:46 -0300

    dev: more code added. pagination in front page is working now. new styles, functions and responsiveness are added in the article page.

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,60 @@
 // pagination
-// const pages = document.querySelectorAll(".pages")! as NodeListOf<HTMLElement>;
+function updateShownPage(pageNumber: number) {
+  elementPageNumber.innerText = pageNumber.toString();
+  let index = pageNumber - 1;
+  pages.forEach((page, i) => {
+    if (page.classList.contains("hide") && i === index || !page.classList.contains("hide") && i !== index) {
+      page.classList.toggle("hide");
+    }
+  });
+}
 
-// const firstPageBtn = document.getElementById(
-//   "firstPageBtn"
-// )! as HTMLImageElement;
+const pages = document.querySelectorAll(".page")! as NodeListOf<HTMLElement>;
+pages.forEach((page) => {
+  page.classList.toggle("hide");
+});
 
-// const previousPageBtn = document.getElementById(
-//   "previousPageBtn"
-// )! as HTMLImageElement;
+const elementPageNumber = document.getElementById(
+  "pageNumber"
+)! as HTMLParagraphElement;
 
-// const nextPageBtn = document.getElementById("nextPageBtn")! as HTMLImageElement;
+const firstPageBtn = document.getElementById(
+  "firstPageBtn"
+)! as HTMLImageElement;
 
-// const lastPageBtn = document.getElementById("lastPageBtn")! as HTMLImageElement;
+const previousPageBtn = document.getElementById(
+  "previousPageBtn"
+)! as HTMLImageElement;
 
-// const pageNumber = document.getElementById(
-//   "pageNumber"
-// )! as HTMLParagraphElement;
+const nextPageBtn = document.getElementById("nextPageBtn")! as HTMLImageElement;
 
-// firstPageBtn.addEventListener("click", () => {
-// });
+const lastPageBtn = document.getElementById("lastPageBtn")! as HTMLImageElement;
 
-// lastPageBtn.addEventListener("click", () => {});
+updateShownPage(+elementPageNumber.innerText);
 
-// previousPageBtn.addEventListener("click", () => {});
+firstPageBtn.addEventListener("click", () => {
+  if (elementPageNumber.innerText !== "1") {
+    updateShownPage(1);
+  }
+});
 
-// nextPageBtn.addEventListener("click", () => {});
+previousPageBtn.addEventListener("click", () => {
+  let actualPage = +elementPageNumber.innerText;
+  if (actualPage > 1) {
+    updateShownPage(actualPage - 1);
+  }
+});
 
+nextPageBtn.addEventListener("click", () => {
+  let actualPage = +elementPageNumber.innerText;
+  if (actualPage < pages.length) {
+    updateShownPage(actualPage + 1);
+  }
+});
 
-
+lastPageBtn.addEventListener("click", () => {
+  let pageNumber = +elementPageNumber.innerText;
+  if (pageNumber < pages.length) {
+    updateShownPage(pages.length);
+  }
+});
